ef8a07d5115ad6b84f22bf67cbf9457160e8d1a9
VishnuMamidi12 <vishnukumarmamidi@gmail.com>
2023-03-15 14:15:29 +0530

    TodoApp

---
+++ todoapp/src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./Todoitem.vue";
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+};
+</script>
